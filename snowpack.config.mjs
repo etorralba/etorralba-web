2730a7ed610374fe37fff6dbb00fb4f0781a356e
etorralba <e.torralba.ba@gmail.com>
2021-08-07 09:05:53 -0600

    Working react App boiler plate

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
+    
   },
   plugins: [
     /* ... */
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
     /* Example: Bundle your final build: */
